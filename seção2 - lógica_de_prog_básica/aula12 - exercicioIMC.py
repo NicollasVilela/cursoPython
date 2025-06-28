eb34014ab9ca4e6cfb19096aa02ab211eeeb9f38
Nicollas <nicollasgvilela@hotmail.com>
2025-06-27 23:54:30 -0300

    a

--- seção2 - lógica_de_prog_básica/aula12 - exercicioIMC.py
+++
@@ -1,8 +0,0 @@
-nome = 'Nicollas Vilela'
-altura = 1.85
-peso = 80
-imc = peso / (altura * altura)
-
-print(nome, 'tem' , altura , 'de altura,',)
-print('pesa', peso, 'kg e seu IMC é',)
-print(round(imc, 2))
